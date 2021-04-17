3111520b39ba026642408b08819deab2fa8d84f4
Arhan Choudhury <arhanchoudhury@gmail.com>
2021-04-17 13:20:13 +0530

    Changes for Ojas

--- public/js/common.js
+++ public/js/common.js
@@ -1,6 +1,18 @@
 //Shared code of the project
 
 $("#postTextarea").keyup((event) => {
-  var TB = $(event.target);
-  console.log(TB);
+  var textbox = $(event.target);
+  var value = textbox.val().trim();
+
+  var submitButton = $("submitPostButton");
+
+  if (submitButton.length == 0) {
+    //Not for the final user
+    return alert("No submit button found");
+  }
+  if (value == "") {
+    submitButton.prop("disabled", true);
+  }
+
+  submitButton.prop("disabled", false);
 });
